c8572b0679867022862e605a3a4fdc0b732195bd
danielbrenners <danbrenners@gmail.com>
2017-10-17 13:47:00 -0700

    websocket connects to unity server

--- public/client.js
+++ public/client.js
@@ -1,12 +1,3 @@
-/*
-$.ajax({
-  type: "GET",
-  url:"/ajax",
-  success: function(data) {
-  }
-});
-*/
-
 var ip = null;
 var connection = null;
 
@@ -26,8 +17,8 @@ $('#enter-room-id').click(function() {
       } else {
         removeSetupUI();
         setupControllerUI();
-        initWS();
         attachEvents();
+        WebSocketSetup(SERVER_IP);
       }
       
     }
@@ -55,41 +46,84 @@ function setupControllerUI(){
 function attachEvents(){
   $('#btn-red').click( function(){
     //console.log(ip);
-    websocket.send('red');
+    WebSocketSendFrame("red");
   });
 
   $('#btn-blue').click( function(){
     //console.log(ip);    
-    websocket.send('blue');
+    WebSocketSendFrame("blue");
   });
 }
 
-//var wsUri = "ws://echo.websocket.org/";
-var wsUri = "ws://echo.websocket.org/";
-var output;
 
-function initWS()
-{
-  output = document.getElementById("output");
-  connectWebSocket();
+
+
+
+
+
+
+
+
+
+//On connect, websocket server should reply back with a unique token to identify client
+var serverToken = "";
+var SERVER_IP = "10.1.250.16";
+var ws;
+
+function WebSocketSendFrame(color) {
+    var wsMessage = {
+        "color": color
+    };
+
+    var jsonString = JSON.stringify(wsMessage)
+
+    ws.send(jsonString);
+
+  console.log("sending " + jsonString);
 }
 
-function connectWebSocket()
-{
-  websocket = new WebSocket(wsUri);
-  websocket.onopen = function(evt) { 
-    console.log('connected');
-  };
-  websocket.onclose = function(evt) {
-    console.log('disconnected');
-  };
-  websocket.onmessage = function(evt) { 
-    console.log(evt.data);
-  };
-  websocket.onerror = function(evt) { 
-    console.log(evt.data);
-  };
+function WebSocketSetup(SERVER_IP) {
+    if ("WebSocket" in window) {    // websocket is supported by the Browser
+
+        // open a web socket
+        ws = new WebSocket("ws://" + SERVER_IP + ":8051/webdata")
+
+        ws.onmessage = function(evt) {
+
+            if (serverToken == "") {
+                serverToken = evt.data;
+                console.log("serverToken set: " + evt.data);
+            } else {
+                console.log("new message received: " + evt.data);
+                var received_msg = evt.data;
+                if (evt.data.substring(0, 1) != "{") {
+                    
+                } else {
+                    //parse json message, do something with it
+                    console.log("RECEIVED NON-JSON FORMATTED MESSAGE")
+                    console.log(evt.data)
+                }
+
+            }
+        };
+
+        ws.onclose = function() {
+            serverToken = "";
+            console.log("Connection is closed...");
+        };
+
+    } else {  
+        alert("WebSocket NOT supported by your Browser!");
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
